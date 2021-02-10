3f563217c7669105b2df2881fbd1d49b94362ce1
Dan Govan <danielgovan@gmail.com>
2021-02-10 22:41:14 +0000

    Cleanup

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./App";
+
+const Router = () => {
+  return (
+    <StyledRouter>
+      <App />
+    </StyledRouter>
+  );
+};
+
+export default Router;
